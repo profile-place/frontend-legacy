12b3395e5d68c78021bf71912b8a485f261d7c83
AndrazKorenc <andy.mann.tk@gmail.com>
2020-08-11 13:29:11 +0200

    Container component

---
+++ components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container section-mod mx-auto p-8 mt-8 mb-8">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Container'
+}
+</script>
+
+<style lang="scss" scoped>
+.section-mod {
+  background: #2C2F33;
+  border: 0;
+  border-radius: 1.5rem;
+}
+</style>
