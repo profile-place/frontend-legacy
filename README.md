2fe105680e95bd6c306d69dbeb2e38c17924b15d
August <august@augu.dev>
2020-09-11 08:54:30 -0700

    Use React instead of Vue

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
-# frontend
+# profile.place | Frontend
+> Frontend repository for [profile.place](https://profile.place), made with React and TailwindCSS
 
-## Build Setup
+## Installation
+### Requirements
+- [API](https://github.com/profile-place/API) instance running
+- Node.js v10 or higher
 
-```bash
-# install dependencies
-$ yarn install
-
-# serve with hot reload at localhost:3000
-$ yarn dev
-
-# build for production and launch server
-$ yarn build
-$ yarn start
-
-# generate static project
-$ yarn generate
-```
-
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+### Process
+:sparkles: ***coming soon?*** :sparkles:
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        greyple: '#99AAB5',
-        blackish: '#23272A',
-        darkish: '#2C2F33'
-      }
-    }
-  },
-  variants: {},
-  plugins: ['@tailwindcss/custom-forms'],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
-}
+	theme: {
+		extend: {
+			colors: {
+				greyple: '#99AAB5',
+				blackish: '#23272A',
+				darkish: '#2C2F33'
+			}
+		}
+	},
+	variants: {},
+	plugins: ['@tailwindcss/custom-forms'],
+	purge: {
+		// Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+			'src/**/*.jsx',
+			'public'
+		]
+	}
+};
